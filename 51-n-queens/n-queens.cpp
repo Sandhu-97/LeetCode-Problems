895398a03a6790e7c157f47afdf788113d96f7c8
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-07-05 19:54:59 +0530

    Time: 11 ms (18.43%) | Memory: 12 MB (25.89%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -2,40 +2,53 @@ class Solution {
 public:
 
 
-    bool isSafe(int row, int col, int n, vector<string>& curr){
+    bool isSafe(int row, int col, int n, vector<string>& curr, unordered_set<int>& vertical, unordered_set<int>& diagonal, unordered_set<int>& anti_diagonal){
         if (row>=n || row<0 || col<0 || col>=n) return false;
-
-        for (int r=row-1;r>=0;r--){
-            if (curr[r][col]=='Q') return false;
-        }
-
-        for (int r=row-1, c=col-1;r>=0 && c>=0;r--, c--){
-            if (curr[r][c]=='Q') return false;
-        }
-        for (int r=row-1, c=col+1;r>=0 && c<n; r--, c++){
-            if (curr[r][c]=='Q') return false;
-        }
+        
+        if (vertical.count(col)) return false;
+        if (diagonal.count(row-col)) return false;
+        if (anti_diagonal.count(row+col)) return false;
         return true;
+        // for (int r=row-1;r>=0;r--){
+        //     if (curr[r][col]=='Q') return false;
+        // }
+        
+        // for (int r=row-1, c=col-1;r>=0 && c>=0;r--, c--){
+        //     if (curr[r][c]=='Q') return false;
+        // }
+        // for (int r=row-1, c=col+1;r>=0 && c<n; r--, c++){
+        //     if (curr[r][c]=='Q') return false;
+        // }
+        // return true;
     }
 
-    void solve(int row, int n, vector<string>& curr, vector<vector<string>>& res){
+    void solve(int row, int n, vector<string>& curr, vector<vector<string>>& res, unordered_set<int>& vertical, unordered_set<int>& diagonal, unordered_set<int>& anti_diagonal){
         if (row>=n){
             res.push_back(curr);
             return;
         }
 
         for (int col=0; col<n;col++){
-            if (isSafe(row, col, n, curr)){
+            if (isSafe(row, col, n, curr, vertical, diagonal, anti_diagonal)){
                 curr[row][col]='Q';
-                solve(row+1, n, curr, res);
+                vertical.insert(col);
+                diagonal.insert(row-col);
+                anti_diagonal.insert(col+row);
+                solve(row+1, n, curr, res, vertical, diagonal, anti_diagonal);
                 curr[row][col] = '.';
+                vertical.erase(col);
+                diagonal.erase(row-col);
+                anti_diagonal.erase(row+col);
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> res;
         vector<string> curr(n, string(n, '.'));
-        solve(0, n, curr, res);
+        unordered_set<int> vertical;
+        unordered_set<int> diagonal;
+        unordered_set<int> anti_diagonal;
+        solve(0, n, curr, res, vertical, diagonal, anti_diagonal);
         return res;
     }
 };
