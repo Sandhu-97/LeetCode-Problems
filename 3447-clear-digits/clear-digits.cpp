380dee38447134bb5cda234641663455c8b130d7
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-07-07 10:31:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (50.84%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        stack<char> st;
+        // stack<char> st;
+        // for (char c: s){
+        //     if (isalpha(c)){
+        //         st.push(c);
+        //     }
+        //     if (isdigit(c) && !st.empty()){
+        //         st.pop();
+        //     }
+        // }
+        // s="";
+        // while (!st.empty()){
+        //     s.push_back(st.top());
+        //     st.pop();
+        // }
+        // reverse(s.begin(), s.end());
+        // return s;
+
+        string res;
         for (char c: s){
-            if (isalpha(c)){
-                st.push(c);
-            }
-            if (isdigit(c) && !st.empty()){
-                st.pop();
-            }
+            if (isalpha(c)) res.push_back(c);
+            if (isdigit(c) && isalpha(res.back())) res.pop_back(); 
         }
-        s="";
-        while (!st.empty()){
-            s.push_back(st.top());
-            st.pop();
-        }
-        reverse(s.begin(), s.end());
-        return s;
+        return res;
     }
 };
