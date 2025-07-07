0fb68b38c6370fae917719f0a60cf7743b316238
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-07-07 10:56:54 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (9.79%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     int minLength(string s) {
-        string res;
+        // string res;
+        // for (char c: s){
+            
+        //     if (c=='B' && res.size()>0 && res.back()=='A' ) res.pop_back();
+        //     else if (c=='D' && res.size()>0 && res.back()=='C') res.pop_back();
+        //     else res.push_back(c);
+        // }
+        // return res.size();
+
+        stack<char> st;
+
         for (char c: s){
             
-            if (c=='B' && res.size()>0 && res.back()=='A' ) res.pop_back();
-            else if (c=='D' && res.size()>0 && res.back()=='C') res.pop_back();
-            else res.push_back(c);
+            if (!st.empty() && c=='B' && st.top()=='A') st.pop();
+            else if (!st.empty() && c=='D' && st.top()=='C') st.pop();
+            
+            else st.push(c);
         }
-        return res.size();
+        return st.size();
     }
 };
