d48a2572311b6d43d6308d36a4122239cce21462
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-07-07 10:57:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (76.35%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int minLength(string s) {
-        // string res;
-        // for (char c: s){
-            
-        //     if (c=='B' && res.size()>0 && res.back()=='A' ) res.pop_back();
-        //     else if (c=='D' && res.size()>0 && res.back()=='C') res.pop_back();
-        //     else res.push_back(c);
-        // }
-        // return res.size();
-
-        stack<char> st;
-
+        string res;
         for (char c: s){
             
-            if (!st.empty() && c=='B' && st.top()=='A') st.pop();
-            else if (!st.empty() && c=='D' && st.top()=='C') st.pop();
-            
-            else st.push(c);
+            if (c=='B' && res.size()>0 && res.back()=='A' ) res.pop_back();
+            else if (c=='D' && res.size()>0 && res.back()=='C') res.pop_back();
+            else res.push_back(c);
         }
-        return st.size();
+        return res.size();
+
+        // stack<char> st;
+
+        // for (char c: s){
+            
+        //     if (!st.empty() && c=='B' && st.top()=='A') st.pop();
+        //     else if (!st.empty() && c=='D' && st.top()=='C') st.pop();
+            
+        //     else st.push(c);
+        // }
+        // return st.size();
     }
 };
