8e3d9f8b5a134d6015380c0afb51a7ddd3aa5302
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-07-10 15:42:58 +0530

    Time: 12 ms (90.30%) | Memory: 95.7 MB (59.64%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
@@ -3,6 +3,7 @@ public:
     int minOperations(vector<int>& nums) {
         unordered_map<int, int> mp;
         for (int i: nums) mp[i]++;
+        
         int count=0;
         for (auto i: mp){
             if (i.second==1) return -1;
