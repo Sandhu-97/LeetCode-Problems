f5222771761ba4db74520fe124b920ae3330ea5f
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-07-22 15:43:52 +0530

    Time: 20 ms (94.03%) | Memory: 16 MB (99.60%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -3,27 +3,24 @@ public:
 
     int m, n;
 
-    void solve(int i, int j, vector<vector<char>>& grid, vector<vector<bool>>& visited){
-        if (i<0 || j<0 || i>=m || j>=n || grid[i][j]!='1' || visited[i][j]) return;
+    void solve(int i, int j, vector<vector<char>>& grid){
+        if (i<0 || j<0 || i>=m || j>=n || grid[i][j]!='1' ) return;
 
-        visited[i][j] = true;
-
-        solve(i+1, j, grid, visited);
-        solve(i-1, j, grid, visited);
-        solve(i, j+1, grid, visited);
-        solve(i, j-1, grid, visited);
+        grid[i][j] = '0';
+        solve(i+1, j, grid);
+        solve(i-1, j, grid);
+        solve(i, j+1, grid);
+        solve(i, j-1, grid);
     }
     int numIslands(vector<vector<char>>& grid) {
         m=grid.size();
         n=grid[0].size();
         
-
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
         int count=0;
         for (int i=0;i<m;i++){
             for (int j=0;j<n;j++){
-                if (grid[i][j]=='1' && !visited[i][j]){
-                    solve(i, j, grid, visited);
+                if (grid[i][j]=='1' ){
+                    solve(i, j, grid);
                     count++;
                 }
             }
