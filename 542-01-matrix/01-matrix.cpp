4065db5ad1c68280059de7ac73b537056d476c97
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-07-23 23:51:57 +0530

    Time: 30 ms (23.44%) | Memory: 36.6 MB (60.86%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,47 +1,40 @@
 class Solution {
 public:
-
-
-
-
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int rows = mat.size();
         int cols = mat[0].size();
-        queue<pair<int, int>> q;
+        queue<pair<int,int>> q;
+        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
 
-        for (int i=0;i<rows;i++){
+        for(int i=0;i<rows;i++){
             for (int j=0;j<cols;j++){
-                if (mat[i][j]==0){
-                    q.push({i,j});
+                if (mat[i][j]==0) {
+                    q.push({i, j});
+                    visited[i][j]=true;
                 }
-                else mat[i][j] = -1;
             }
         }
 
         int dx[] = {1,-1,0,0};
         int dy[] = {0,0,1,-1};
+
         while (!q.empty()){
-            int size=q.size();
 
-            for (int i=0;i<size;i++){
-                auto front = q.front();
-                q.pop();
-
-                int row = front.first;
-                int col = front.second;
-
-                for (int i=0;i<4;i++){
-                    int newrow = row+dx[i];
-                    int newcol = col+dy[i];
-
-                    if (newrow >= 0 && newrow < rows && newcol >= 0 && newcol < cols && mat[newrow][newcol]==-1){
-                        q.push({newrow, newcol});
-                        mat[newrow][newcol] = mat[row][col] + 1;
-                    }
+            auto front = q.front();
+            q.pop();
+            int row = front.first;
+            int col = front.second;
+            for (int i=0;i<4;i++){
+                int newrow = row+dx[i];
+                int newcol = col+dy[i];
+                if (newrow>=0 && newrow < rows && newcol>=0 && newcol<cols && !visited[newrow][newcol] && mat[newrow][newcol]!=0){
+                    q.push({newrow, newcol});
+                    visited[newrow][newcol] = true;
+                    mat[newrow][newcol] = mat[row][col]+1;
                 }
             }
+            
         }
         return mat;
-         
     }
 };
