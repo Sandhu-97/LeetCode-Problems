70ec3b214cfb516e8c6cda5da579d2dd88cc1098
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-07-24 14:49:22 +0530

    Time: 14 ms (11.02%) | Memory: 24.1 MB (5.12%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
+
+    bool isCycleDFS(int u, vector<vector<int>>& adj, vector<bool>& visited, vector<bool> stack){
+        visited[u] = true;
+        stack[u] = true;
+
+        for (auto v: adj[u]){
+            if (!visited[v] && isCycleDFS(v, adj, visited, stack)) return true;
+            else if (stack[v]) return true;
+        }
+        stack[u] = false;
+        return false;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        vector<int> indegree(numCourses, 0);
-        queue<int> q;
+        vector<bool> visited(numCourses);
+        vector<bool> stack(numCourses);
 
-        int count=0;
 
         for (auto edge: prerequisites){
             adj[edge[1]].push_back(edge[0]);
-            indegree[edge[0]]++;
         }
 
-        for (int i=0;i<numCourses;i++) {
-            if (indegree[i]==0) {
-                q.push(i);
+        for (int i=0;i<numCourses;i++){
+            if (!visited[i]){
+                if (isCycleDFS(i, adj, visited, stack)) return false;
             }
         }
+        return true;
 
 
-        while (!q.empty()){
-            int front = q.front();
-            q.pop();
-            count++;
 
-            for (auto v: adj[front]){
-                indegree[v]--;
-                if (indegree[v]==0) q.push(v);
-            }
-        }
-
-        return count==numCourses;
+        
         
     }
 };
