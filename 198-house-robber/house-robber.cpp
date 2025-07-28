5f4a9aa5568a37603d6e3e3693b0d6731917c741
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-07-28 10:50:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (22.59%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-
-    int solve(int i, vector<int>& nums, vector<int>& dp){
-        if (i>=nums.size()){
-            return 0;
-        }
-        if (dp[i]!=-1) return dp[i];
-
-        int steal = nums[i] + solve(i+2, nums, dp);
-        int leave = solve(i+1, nums, dp);
-
-        return dp[i]= max(steal, leave);
-    }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size()+1, -1);
-        return solve(0, nums, dp);
+        int n = nums.size();
+        if (n==0) return 0;
+        if (n==1) return nums[0];
+
+
+        vector<int> dp(n, -1);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+
+        for (int i=2;i<n;i++){
+            int steal = nums[i]+dp[i-2];
+            int leave = dp[i-1];
+
+            dp[i] = max(steal, leave);
+        }
+
+        return dp[n-1];
+
+
     }
 };
