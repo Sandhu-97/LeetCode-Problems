e80fcdf4c96df24d5fd1c9eaea7f0eb5de9133f3
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-07-28 10:04:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (36.84%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-
-    int memo(int n, vector<int>& dp){
-        if (n==0||n==1) return 1;
-
-        if (dp[n] != -1) return dp[n];
-
-        return dp[n] = memo(n-1, dp) + memo(n-2, dp);
-    }
     int climbStairs(int n) {
-        if (n==0 || n==1) return 1;
+        //  TABULATION
+
         vector<int> dp(n+1, -1);
-        return memo(n, dp);
+        dp[0]=1;
+        dp[1]=1;
+        for (int i=2;i<=n;i++){
+            dp[i]= dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
     }
 };
