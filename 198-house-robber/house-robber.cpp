1b7912326518ba945f17436aba37de8f30539e6a
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-08-04 22:26:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (99.88%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
+    // CONSTANT SPACE
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp (n+1, -1);
 
         if (n==0) return 0;
         if (n==1) return nums[0];
 
-        dp[0] = 0;
-        dp[1] = nums[0];
-
-        for (int i=2;i<=n;i++){
-            dp[i] = max(nums[i-1] + dp[i-2], dp[i-1]);
+        int prevprev = 0;
+        int prev = nums[0];
+        int profit = INT_MIN;
+        for (int i=1;i<n;i++){
+            profit = max(nums[i] + prevprev, prev); 
+            prevprev = prev;
+            prev = profit;
         }
 
-        return dp[n];
-        
+        return prev;
     }
 };
