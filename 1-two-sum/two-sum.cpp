d808319ae32c34f0cf35ce7870fedaaf54eef3d8
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-08-24 12:50:29 +0530

    Time: 4 ms (57.24%) | Memory: 14.7 MB (50.28%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,13 +4,13 @@ public:
         unordered_map<int, int> mp;
 
         for (int i=0;i<nums.size();i++){
-            int diff = target-nums[i];
+            int diff = target - nums[i];
 
-            if (mp.find(diff)!=mp.end()){
-                return {mp[diff], i};
-            }
+            if (mp.count(diff)) return {mp[diff], i};
+
             mp[nums[i]] = i;
         }
-        return {-1,-1};
+
+        return {};
     }
 };
