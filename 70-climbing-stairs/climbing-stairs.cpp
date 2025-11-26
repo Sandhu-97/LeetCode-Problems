a5480fe84b2f2678a29cc33d9f3a47eb70cd359e
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-11-26 10:32:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (86.57%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-// TABULATION
-    int climbStairs(int n) {
-        if (n==1 || n==0) return 1;
-        vector<int> dp(n+1, 0);
-        dp[0] = 1;
-        dp[1] = 1;
 
-        for (int i=2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+    // SPACE OPTIMIZED
+    int climbStairs(int n) {
+        if (n<=3) return n;
+        int first = 2;
+        int second = 3;
+        for (int i=4;i<=n;i++){
+            int temp = first+second;
+            first = second;
+            second = temp;
         }
 
-        return dp[n];
-
+        return second;
     }
 };
