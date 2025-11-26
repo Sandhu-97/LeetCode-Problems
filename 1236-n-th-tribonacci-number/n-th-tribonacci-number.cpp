f0045b3f28e6028eb10adbfaf20558efd241ed44
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-11-26 11:05:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (65.69%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
 
-    // TABULATION
+    // SPACE OPTIMIZED
     int tribonacci(int n) {
         if (n==0) return 0;
         if (n<=2) return 1;
+        int first = 0;
+        int second = 1;
+        int third = 1;
+        for (int i=3;i<=n;i++) {
+            int temp = first + second + third;
+            first = second;
+            second = third;
+            third = temp;
+        }
 
-        int dp[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
-        for (int i=3;i<=n;i++) dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
-
-        return dp[n];
+        return third;
     }
 };
