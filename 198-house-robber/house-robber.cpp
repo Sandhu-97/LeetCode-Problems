21825c1205229bf4127f7a995d6b5c1023dac979
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-12-08 10:38:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (44.81%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    // tabulation
+    int solve(int i, vector<int>& nums, vector<int>& dp){
+        if (i>=nums.size()) return 0;
+        if (dp[i]!=-1) return dp[i];
+        int rob = nums[i] + solve(i+2, nums, dp);
+        int skip = solve(i+1, nums,dp);
+        return dp[i] = max(rob, skip);
+    }
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n<2){
-            return nums[0];
-        }
-        vector<int> dp (n, 0);
-        dp[0] = nums[0];
-        dp[1] = max(dp[0], nums[1]);
-
-        for (int i=2;i<n;i++){
-            dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
-        }
-
-        return max(dp[n-1], dp[n-2]);
+        vector<int> dp(nums.size(), -1);
+        return solve(0, nums, dp);    
     }
 };
