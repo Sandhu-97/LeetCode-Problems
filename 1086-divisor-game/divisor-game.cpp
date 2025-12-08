34c4ea8c740acd9cb0efaa53dd26ce41708a0d00
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-12-08 12:53:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (99.38%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
-    int solve(int n, vector<int>& dp){
-        if (n<=1) return 0;
-        if (dp[n]!=-1) return dp[n];
-
-        for (int i=1;i<n;i++){
-            if (n%i==0){
-                if (solve(n-i, dp)==0){
-                    dp[n] = 1;
-                    return 1;
-                }
-            }
-        }
-        dp[n] = 0;
-        return 0;
-    }
     bool divisorGame(int n) {
-        vector<int> dp(n+1, -1);
-        return solve(n, dp);
+        return n%2==0;
     }
 };
