1b0a6943f70e1f8eb6b604d47f631bde2733e8b1
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-12-24 17:23:34 +0530

    Time: 3 ms (53.70%) | Memory: 23.7 MB (71.30%) - LeetSync

--- 3843-partition-array-into-two-equal-product-subsets/partition-array-into-two-equal-product-subsets.cpp
+++ 3843-partition-array-into-two-equal-product-subsets/partition-array-into-two-equal-product-subsets.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
+    bool solve(int i, long long p1, long long p2, vector<int>& nums, long long target){
 
-    bool solve(int i, long long p1, long long p2, long long target, vector<int>& nums){
-        if (p1>target || p2 > target) return false;
-        if (i==nums.size()) return p1==target && p2==target;
-        bool a = solve(i+1, p1*nums[i], p2, target, nums);
-        bool b = solve(i+1, p1, p2*nums[i], target, nums);
+        if (p1==target && p2==target && nums.size()==i) return true;
+        if (p1>target || p2>target || i>=nums.size()) return false;
+
+
+        bool a = solve(i+1, p1*nums[i], p2, nums, target);
+        bool b = solve(i+1, p1, p2*nums[i], nums, target);
+
         return a | b;
     }
     bool checkEqualPartitions(vector<int>& nums, long long target) {
-        return solve(0, 1, 1, target, nums);
+        return solve(0, 1, 1, nums, target);
     }
 };
