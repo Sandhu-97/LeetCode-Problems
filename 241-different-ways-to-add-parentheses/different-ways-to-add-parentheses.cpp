f3741b561ffe9e54abe02515f3c2107b88869fac
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-12-27 12:50:38 +0530

    Time: 4 ms (26.24%) | Memory: 14.1 MB (23.37%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    vector<int> diffWaysToCompute(string exp) {
+    vector<int> solve(string exp){
         vector<int> res;
 
         for (int i=0;i<exp.size();i++){
-            if (exp[i] == '+' || exp[i] == '-' || exp[i] == '*'){
-                vector<int> left = diffWaysToCompute(exp.substr(0, i));
-                vector<int> right = diffWaysToCompute(exp.substr(i+1));
+            if (exp[i]=='+' || exp[i]=='-' || exp[i]=='*'){
+                vector<int> left = solve(exp.substr(0, i));
+                vector<int> right = solve(exp.substr(i+1));
 
                 for (int x: left){
                     for (int y: right){
-                        if (exp[i]=='+') res.push_back(x+y); 
-                        if (exp[i]=='-') res.push_back(x-y); 
-                        if (exp[i]=='*') res.push_back(x*y); 
+                        if (exp[i]=='+') res.push_back(x+y);
+                        else if (exp[i]=='*') res.push_back(x*y);
+                        else if (exp[i]=='-') res.push_back(x-y);
+                    
                     }
                 }
             }
         }
         if (res.empty()) res.push_back(stoi(exp));
-
         return res;
+        
+    }
+    vector<int> diffWaysToCompute(string expression) {
+        return solve(expression);   
     }
 };
