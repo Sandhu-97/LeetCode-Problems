00f19e31c559ee0c77d641fe16a6b333ec6a3dc9
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-01-02 23:42:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (85.98%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+    int dp[21][21];
     int solve(int i, int j, vector<int>& nums){
         if (i==j) return nums[i];
+        if (dp[i][j]!=-1) return dp[i][j];
         int left = nums[i] - solve(i+1, j, nums);
         int right = nums[j] - solve(i, j-1, nums);
-        return max(left, right);
+        return dp[i][j] = max(left, right);
         
     }
     bool predictTheWinner(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
         return solve(0, nums.size()-1, nums) >=0;
     }
 };
