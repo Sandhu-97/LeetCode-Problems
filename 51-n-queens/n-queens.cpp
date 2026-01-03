e2de51a37a49e2db9eff6fbba2357ed731b77109
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-01-03 23:04:37 +0530

    Time: 3 ms (62.89%) | Memory: 10.2 MB (75.54%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
 
-
-    bool isSafe(int row, int col, int n, vector<string>& curr){
-        if(row>= n || row<0 || col < 0 || col>=n) return false;
-        for (int i=row-1;i>=0;i--){
-            if (curr[i][col] == 'Q') return false;
-        }
-
-        for (int i=row-1, j=col-1; i>=0 && j>=0; i--, j--){
-            if (curr[i][j]=='Q') return false;
-        } 
-        for (int i=row-1, j=col+1;i>=0 && j<n; i--, j++) if (curr[i][j]=='Q') return false;
-
+    bool valid(int row, int col, int n, vector<string>& curr){
+        if (row>=n || row<0 || col>=n || col<0) return false;
+        for (int i=row-1;i>=0;i--) if (curr[i][col]=='Q') return false;
+        for (int i=row-1, j=col-1; i>=0 && j>=0; i--, j--) if (curr[i][j]=='Q') return false;
+        for (int i=row-1, j=col+1; i>=0 && j<n; i--, j++) if (curr[i][j]=='Q') return false;
         return true;
     }
-    void solve(int i, int n, vector<string>& curr, vector<vector<string>>& ans){
-        if (i>=n){
+
+    void solve(int row, int n, vector<string>& curr, vector<vector<string>>& ans){
+        if (row>=n){
             ans.push_back(curr);
             return;
         }
-        for (int j=0;j<n;j++){
-            if (isSafe(i, j, n, curr)){
-                curr[i][j] = 'Q';
-                solve(i+1, n, curr, ans);
-                curr[i][j] = '.';
+
+        for (int col=0;col<n;col++){
+            if (valid(row, col, n, curr)){
+                curr[row][col] = 'Q';
+                solve(row+1, n, curr, ans);
+                curr[row][col] = '.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
         vector<string> curr(n, string(n, '.'));
+        vector<vector<string>> ans;
         solve(0, n, curr, ans);
         return ans;
     }
