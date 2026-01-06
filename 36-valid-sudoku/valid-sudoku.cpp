27d991932fe9366fc6afd0fff0351966ef90b4ca
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-01-06 19:20:27 +0530

    Time: 7 ms (30.72%) | Memory: 23 MB (57.31%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        set<string> s;
-        for (int i=0;i<9;i++){
-            for (int j=0;j<9;j++){
-                if (board[i][j]=='.') continue;
-                string row = string(1, board[i][j])+"row"+to_string(i);
-                string col = string(1, board[i][j])+"col"+to_string(j);
-                string box = string(1, board[i][j])+"box"+to_string(i/3)+to_string(j/3);
 
-                if (s.count(row) || s.count (col) || s.count(box)) return false;
-                s.insert(row);
-                s.insert(col);
-                s.insert(box);
+        set<char> st;
+        for (int row=0;row<9;row++){
+            for (int col=0;col<9;col++){
+                if (board[row][col]!='.'){
+                    if (st.find(board[row][col]) != st.end()) return false;
+                    st.insert(board[row][col]);
+                }
             }
+            st.clear();
         }
+
+        for (int col=0;col<9;col++){
+            for (int row=0;row<9;row++){
+                if (board[row][col]!='.') {
+                    if (st.find(board[row][col]) !=st.end()) return false;
+                    st.insert(board[row][col]);
+                }
+            }
+            st.clear();
+        }
+        for (int row=0;row<9;row+=3){
+            for (int col=0;col<9;col+=3){
+                st.clear();
+                for (int i=0;i<3;i++){
+                    for (int j=0;j<3;j++){
+                        char c = board[row+i][col+j];
+                        if (c=='.') continue;
+                        if (st.count(c)) return false;
+                        st.insert(c);
+                    }
+                }
+            }
+            st.clear();
+        }
+
         return true;
+
     }
 };
