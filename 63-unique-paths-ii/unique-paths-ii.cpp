23a703db378fe687744ce9df0b35bd5484b59a3a
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-01-22 12:30:00 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (6.20%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> grid, dp;
-    int m,n;
-    int solve (int i, int j){
-        if (i>=m || j>=n || grid[i][j]==1) return 0;
-        if (i==m-1 && j==n-1) return 1;
-        if (dp[i][j]!=-1) return dp[i][j];
-
-        return dp[i][j] = solve(i+1, j) + solve(i, j+1);
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        grid = obstacleGrid;
-        m=grid.size();
-        n=grid[0].size();
-        dp.assign(m, vector<int>(n, -1));
-        return solve(0,0);
+        vector<vector<int>> grid = obstacleGrid;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<uint>> dp(m+1, vector<uint>(n+1, 0));
+        dp[m-1][n-1] = 1;
+        for (int i=m-1;i>=0;i--){
+            for (int j=n-1;j>=0;j--){
+                if (grid[i][j]==1) dp[i][j]=0;
+                else dp[i][j] += dp[i+1][j] + dp[i][j+1];
+            }
+        }
+
+        return dp[0][0];
     }
 };
