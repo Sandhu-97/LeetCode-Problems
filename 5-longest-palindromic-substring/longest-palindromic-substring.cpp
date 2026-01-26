870df19edf555e455dbbf2e03d9a97b916d40430
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-01-26 14:38:25 +0530

    Time: 1077 ms (5.00%) | Memory: 13.6 MB (49.02%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -11,6 +11,7 @@ public:
     string longestPalindrome(string s) {
         int maxi=0;
         string ans="";
+
         for (int i=0;i<s.size();i++){
             for (int j=i;j<s.size();j++){
                 int l=i, r=j;
