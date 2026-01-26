f6de1b01b6cf2f11f5829f569d3948bfa055f465
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-01-26 21:52:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (84.98%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int ans = INT_MIN;
+        int n=nums.size();
+        int prod=1;
+        for (int i=0;i<n;i++){
+            prod*=nums[i];
+            ans = max(prod, ans);
+            if (prod==0){
+                prod=1;
+            }
+        }
+        prod=1;
+        for (int i=n-1;i>=0;i--){
+            prod*=nums[i];
+            ans = max(prod, ans);
+            if (prod==0) prod=1;
+        }
+        return ans;
+    }
+};
