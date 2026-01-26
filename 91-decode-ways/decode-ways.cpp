6f56893a77f64ec4c8f50fd4bde35ca6e33636dc
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-01-26 18:36:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (67.22%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    int n;
-    vector<int> dp;
-    int solve(int i, string s){
-        if (i>=n) return 1;
-        if (s[i]=='0') return 0;
-        if (dp[i]!=-1) return dp[i];
-        int ways = solve(i+1, s);
-        if(i+1<n){
-            if (s[i]=='1' || (s[i]=='2' && s[i+1]<='6')){
-                ways+=solve(i+2, s);
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        dp[n] = 1;
+        for (int i=n-1;i>=0;i--){
+            if (s[i]=='0') dp[i]=0;
+            else {
+                dp[i] = dp[i+1];
+                if (i+1<n && (s[i]=='1'  || (s[i]=='2' && s[i+1]<='6'))){
+                    dp[i] += dp[i+2];
+                }
             }
         }
-        return dp[i]=ways;
-
-    }
-    int numDecodings(string s) {
-        n=s.size();
-        dp.assign(n+1, -1);
-        return solve(0, s);
+        return dp[0];
     }
 };
