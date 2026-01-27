b7850d65ad196e18935bf948b87ceb4f7329365b
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-01-27 16:16:24 +0530

    Time: 95 ms (71.46%) | Memory: 110.5 MB (39.38%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,15 +1,22 @@
-class Solution {
+class Solution {    
 public:
-    bool solve(int i, vector<int>& nums, int target, vector<vector<int>>& dp){
+
+    vector<vector<int>> dp;
+
+    bool solve(int i, int target, vector<int>& nums){
+        if (i>=nums.size()) return false;
         if (target==0) return true;
-        if (i==nums.size() || target<0) return false;
+        if (target<0) return false;
         if (dp[i][target]!=-1) return dp[i][target];
-        return dp[i][target] = solve(i+1, nums, target-nums[i], dp) || solve(i+1, nums, target, dp);
+        dp[i][target] = solve(i+1, target, nums) || solve(i+1, target-nums[i], nums);
+        return dp[i][target];
     }
     bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int sum=0;
+        for (int i: nums) sum+=i;
         if (sum%2!=0) return false;
-        vector<vector<int>>dp (nums.size(), vector<int>(sum+1, -1));
-        return solve(0, nums, sum/2, dp);
+        int target=sum/2;
+        dp.assign(nums.size(), vector<int>(target+1, -1));
+        return solve(0, target, nums);
     }
 };
