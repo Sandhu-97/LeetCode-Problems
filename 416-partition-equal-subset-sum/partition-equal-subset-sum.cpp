a3afcba927b7631eb8e9bbf123b8f319d3ee998f
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-01-29 09:52:37 +0530

    Time: 91 ms (72.74%) | Memory: 110.6 MB (32.41%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,21 +1,17 @@
-class Solution {    
+class Solution {
 public:
-
     vector<vector<int>> dp;
-
     bool solve(int i, int target, vector<int>& nums){
         if (i>=nums.size()) return false;
         if (target==0) return true;
         if (target<0) return false;
         if (dp[i][target]!=-1) return dp[i][target];
-        dp[i][target] = solve(i+1, target, nums) || solve(i+1, target-nums[i], nums);
-        return dp[i][target];
+        return dp[i][target]=solve(i+1, target, nums) || solve(i+1, target-nums[i], nums);
     }
     bool canPartition(vector<int>& nums) {
-        int sum=0;
-        for (int i: nums) sum+=i;
-        if (sum%2!=0) return false;
-        int target=sum/2;
+        int target=accumulate(nums.begin(), nums.end(), 0);
+        if (target%2!=0) return false;
+        target/=2;
         dp.assign(nums.size(), vector<int>(target+1, -1));
         return solve(0, target, nums);
     }
