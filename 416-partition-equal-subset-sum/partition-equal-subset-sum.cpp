7d9091dc3c45785ab332d089482363e68693a7ba
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-01-29 10:15:27 +0530

    Time: 88 ms (73.64%) | Memory: 13.4 MB (86.71%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -6,22 +6,14 @@ public:
         if (target%2!=0) return false;
         target/=2;
         int n=nums.size();
-        vector<vector<bool>> dp(n+1, vector<bool>(target+1, false));
+        vector<bool> dp(target+1, false);
+        dp[0]=true;
 
-        for (int i=0;i<=n;i++){
-            dp[i][0] = true;
-        }
-
-        for (int i=1;i<=n;i++){
-            for (int j=1;j<=target;j++){
-                if (nums[i-1] <= j){
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
-                }
-                else dp[i][j]=dp[i-1][j];
+        for (int i=0;i<n;i++){
+            for (int j=target;j>=nums[i];j--){
+                dp[j] = dp[j] || dp[j-nums[i]];
             }
         }
-
-        return dp[n][target];
-        
+        return dp[target];
     }
 };
