d3e0bb58436db6f064d1a05f371a1df36879c434
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-01-31 13:10:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (90.21%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
-    int m;
-    vector<vector<int>> dp;
-    int solve (int i, int j, vector<vector<int>>& triangle){
-        if (i>=m) return 0;
-        if (j<0 || j>=triangle[i].size()) return 1e5;
-        if (dp[i][j]!=INT_MAX) return dp[i][j];
-        int path1 = solve(i+1, j, triangle);
-        int path2 = solve(i+1, j+1, triangle);
-
-        return dp[i][j] = triangle[i][j] + min(path1, path2);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
-        m=triangle.size();
-        dp.assign(m, vector<int>(0));
-        for (int i=0;i<m;i++){
-            dp[i].resize(triangle[i].size(), INT_MAX);
+        int n = triangle.size()-1;
+
+        for (int row=n-1;row>=0;row--){
+            for (int col=0;col<triangle[row].size();col++){
+                triangle[row][col] += min(triangle[row+1][col], triangle[row+1][col+1]);
+            }
         }
-        return solve(0,0,triangle);
+        return triangle[0][0];
     }
 };
