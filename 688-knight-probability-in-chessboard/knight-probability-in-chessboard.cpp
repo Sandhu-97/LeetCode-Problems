fae35a5e52b050de9b66caa05b01af59f77b9d68
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-02-02 10:07:59 +0530

    Time: 3 ms (94.55%) | Memory: 8.7 MB (90.01%) - LeetSync

--- 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.cpp
@@ -13,9 +13,9 @@ public:
     double solve(int row, int col, int k){
         if(!valid(row, col)) return 0.0;
         if (k==0) return 1.0;
-        if (dp[row][col][k]!=-1.0) return dp[row][col][k];
+        if (dp[row][col][k]) return dp[row][col][k];
         double prob = 0;
-        for (auto i: dir){
+        for (auto& i: dir){
             prob += solve(row+i[0], col+i[1], k-1);
         }
         return dp[row][col][k] = prob/8.0;
@@ -24,11 +24,11 @@ public:
     }
     double knightProbability(int n, int k, int row, int column) {
         N=n;
-        for (int i=0;i<26;i++){
-            for (int j=0;j<26;j++){
-                for (int k=0;k<101;k++) dp[i][j][k] = -1.0;
-            }
-        }
+        // for (int i=0;i<26;i++){
+        //     for (int j=0;j<26;j++){
+        //         for (int k=0;k<101;k++) dp[i][j][k] = -1.0;
+        //     }
+        // }
         double ans = solve(row, column, k);
         return ans;
     }
