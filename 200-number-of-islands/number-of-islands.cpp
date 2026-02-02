3b40696443b57471bd29c790b9092d8e77816b78
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-02-02 14:20:33 +0530

    Time: 22 ms (88.07%) | Memory: 16.4 MB (68.49%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-
     int m, n;
 
-    void solve(int i, int j, vector<vector<char>>& grid){
-        if (i<0 || j<0 || i>=m || j>=n || grid[i][j]!='1' ) return;
+    void solve(int row, int col, vector<vector<char>>& grid){
+        if (row<0 || col<0 || row>=m || col>=n) return;
+        if (grid[row][col]=='0') return;
 
-        grid[i][j] = '0';
-        solve(i+1, j, grid);
-        solve(i-1, j, grid);
-        solve(i, j+1, grid);
-        solve(i, j-1, grid);
+        grid[row][col]='0';
+        solve(row+1, col, grid);
+        solve(row-1, col, grid);
+        solve(row, col+1, grid);
+        solve(row, col-1, grid);
     }
+
     int numIslands(vector<vector<char>>& grid) {
-        m=grid.size();
-        n=grid[0].size();
-        
+        m = grid.size();
+        n = grid[0].size();
+
         int count=0;
         for (int i=0;i<m;i++){
             for (int j=0;j<n;j++){
-                if (grid[i][j]=='1' ){
+                if (grid[i][j]=='1') {
                     solve(i, j, grid);
                     count++;
                 }
