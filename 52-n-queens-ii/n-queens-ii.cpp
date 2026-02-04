17dadf29b3387a29c9436ebaf96af485c9f8235d
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-02-04 11:00:05 +0530

    Time: 11 ms (8.91%) | Memory: 11.2 MB (7.58%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.cpp
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -4,11 +4,21 @@ public:
     int n;
     vector<string> curr;
     int count;
+    set<int> cols;
+    set<int> diagonals;
+    set<int> antidiagonals;
+    // bool valid (int row, int col){
+    //     if (row<0 || row>=n || col<0 || col>=n ) return false;
+    //     for (int i=row-1 ; i>=0 ; i--) if (curr[i][col]=='Q') return false;
+    //     for (int i=row-1, j=col-1 ; i>=0 && j>=0 ; i--, j--) if (curr[i][j]=='Q') return false;
+    //     for (int i=row-1, j=col+1 ; i>=0 && j<n ; i--, j++) if (curr[i][j]=='Q') return false;
+    //     return true;
+    // }
     bool valid (int row, int col){
         if (row<0 || row>=n || col<0 || col>=n ) return false;
-        for (int i=row-1 ; i>=0 ; i--) if (curr[i][col]=='Q') return false;
-        for (int i=row-1, j=col-1 ; i>=0 && j>=0 ; i--, j--) if (curr[i][j]=='Q') return false;
-        for (int i=row-1, j=col+1 ; i>=0 && j<n ; i--, j++) if (curr[i][j]=='Q') return false;
+        if (cols.find(col)!=cols.end()) return false;
+        if (diagonals.find(row-col) != diagonals.end()) return false;
+        if (antidiagonals.find(row+col) != antidiagonals.end()) return false;
         return true;
     }
 
@@ -21,8 +31,14 @@ public:
         for (int col=0;col<n;col++){
             if (valid(row, col)){
                 curr[row][col] = 'Q';
+                cols.insert(col);
+                diagonals.insert(row-col);
+                antidiagonals.insert(row+col);
                 solve(row+1);
                 curr[row][col] = '.';
+                cols.erase(col);
+                diagonals.erase(row-col);
+                antidiagonals.erase(row+col);
             }
         }
     }
