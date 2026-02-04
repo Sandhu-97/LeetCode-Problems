ac4e5023685a91e0801109c50daf7de18f49f606
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-02-04 11:10:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (53.88%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.cpp
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -4,15 +4,13 @@ public:
     int n;
     vector<string> curr;
     int count;
-    unordered_set<int> cols;
-    unordered_set<int> diagonals;
-    unordered_set<int> antidiagonals;
+    vector<bool> cols;
+    vector<bool> diagonals;
+    vector<bool> antidiagonals;
 
     bool valid (int row, int col){
         if (row<0 || row>=n || col<0 || col>=n ) return false;
-        if (cols.find(col)!=cols.end()) return false;
-        if (diagonals.find(row-col) != diagonals.end()) return false;
-        if (antidiagonals.find(row+col) != antidiagonals.end()) return false;
+        if (cols[col] || diagonals[n-1+row-col] || antidiagonals[row+col]) return false;
         return true;
     }
 
@@ -25,14 +23,14 @@ public:
         for (int col=0;col<n;col++){
             if (valid(row, col)){
                 curr[row][col] = 'Q';
-                cols.insert(col);
-                diagonals.insert(row-col);
-                antidiagonals.insert(row+col);
+                cols[col] = true;
+                diagonals[n-1+row-col] = true;
+                antidiagonals[row+col] = true;;
                 solve(row+1);
                 curr[row][col] = '.';
-                cols.erase(col);
-                diagonals.erase(row-col);
-                antidiagonals.erase(row+col);
+                cols[col] = false;;
+                diagonals[n-1+row-col] = false;
+                antidiagonals[row+col] = false;
             }
         }
     }
@@ -40,6 +38,9 @@ public:
         n=N;
         curr.assign(n, string(n, '.'));
         count=0;
+        cols.assign(n, false);
+        diagonals.assign(2*n-1, false);
+        antidiagonals.assign(2*n-1, false);
         solve(0);
         return count;
     }
