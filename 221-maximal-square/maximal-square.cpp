da4c6f9449cffc282bec812a2409ce1d5679213f
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-02-05 13:35:30 +0530

    Time: 31 ms (14.79%) | Memory: 31.2 MB (10.56%) - LeetSync

---
+++ 221-maximal-square/maximal-square.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int m,n;
+    vector<vector<int>> dp;
+    int solve(int i, int j, vector<vector<char>>& matrix){
+        if (i>=m || j>=n || matrix[i][j]=='0') return 0;
+        if (dp[i][j]!=-1) return dp[i][j];
+
+        int right = solve(i, j+1, matrix);
+        int down = solve(i+1, j, matrix);
+        int diag = solve(i+1, j+1, matrix);
+
+        return dp[i][j] = 1 + min( {right, down, diag} );
+    }
+
+    int maximalSquare(vector<vector<char>>& matrix) {
+        m=matrix.size();
+        n=matrix[0].size();
+        dp.assign(m, vector<int>(n, -1));
+        int ans = 0;
+        for (int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                ans = max(ans, solve(i, j, matrix));   
+            }
+        }
+        return ans*ans;
+
+
+    }
+};
