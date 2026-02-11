6c96f7f11951f1f5d77e05ed4b7e911ed8ba2ee7
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-02-11 11:48:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (93.04%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int n, sum;
-    vector<vector<int>> dp;
-
-    int solve(int i, int diff, vector<int>& stones){
-        if (i==n) return abs(diff);
-
-        int key = diff+sum;
-        if(dp[i][key]!=-1) return dp[i][key];
-
-        int set1 = solve(i+1, diff+stones[i], stones);
-        int set2 = solve(i+1, diff-stones[i], stones);
-
-        return dp[i][key] = min(set1, set2);
-        
-    }
     int lastStoneWeightII(vector<int>& stones) {
-        n = stones.size();
-        sum = accumulate(stones.begin(), stones.end(), 0);
-        dp.assign(n, vector<int> (2*sum+1, -1));
+        int sum = accumulate(stones.begin(), stones.end(), 0);
+        int n = stones.size();
+        int target = sum/2;
 
-        return solve(0, 0, stones);
+        vector<bool> dp(target+1, false);
+        dp[0] = true;
+
+        for (int w: stones){
+            for (int x = target;x>=w;x--){
+                dp[x] = dp[x] || dp[x-w];
+            }
+        }
+        int ans = 0;
+        for (int x = target;x>=0;x--){
+            if (dp[x]){
+                ans=x;
+                break;
+            }
+        }
+        return sum-2*ans;
+
     }
 };
