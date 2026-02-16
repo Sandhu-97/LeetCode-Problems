6222f422d54ac5bd031185915f9df62bb213c3a8
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-02-16 13:14:49 +0530

    Time: 4 ms (29.28%) | Memory: 8.3 MB (15.72%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        if (n==0) return 0;
+
+        int ans = 0;
+
+        for (int i=0;i<32;i++){
+            ans<<=1;
+            
+            ans = (ans | (n&1));
+
+            n >>= 1;
+        }
+        return ans;
+    }
+};
