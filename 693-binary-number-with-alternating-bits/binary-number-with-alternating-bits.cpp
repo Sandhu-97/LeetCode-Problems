2f580705906b70168931acbcd603db2246b49258
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-02-18 10:23:41 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (62.63%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
@@ -7,7 +7,6 @@ public:
             int bit = n&1;
             if (bit!=expected) return false;
             expected = 1 - bit;
-            cout << bit << endl;
             n>>=1;
         }
         return true;
