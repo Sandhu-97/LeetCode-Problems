08eeccdce0e484df28db44befb9cbe6bf110dec5
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-02-20 10:44:55 +0530

    Time: 254 ms (6.70%) | Memory: 83.4 MB (5.30%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int n;
+    bool valid (int i, int j, vector<vector<int>>& grid){
+        if (i<0 || j<0 || i>= n || j>=n || grid[i][j]!=0) return false;
+        return true;
+    }
+
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        n = grid.size();
+        if (grid[0][0]!=0) return -1;
+        queue<pair<int, int>> q;
+        q.push({0,0});
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        vector<vector<int>> dir = {
+            {1, 0}, {-1, 0}, {0, 1}, {0, -1},
+            {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
+        };
+        visited[0][0] = true;
+        int ans = 1;
+        while (!q.empty()){
+            int size=q.size();
+
+            while (size--){
+                auto [r, c] = q.front(); q.pop();
+
+                if (r==n-1 && c==n-1) return ans;
+                for (auto d: dir){
+                    int nrow = r+d[0];
+                    int ncol = c+d[1];
+                    if (valid(nrow, ncol, grid)){
+                        if (visited[nrow][ncol]) continue;
+                        visited[nrow][ncol] = true;
+                        q.push({nrow, ncol});
+                    }
+                }
+            }
+            ans++;
+        }
+        return -1;
+    }
+};
