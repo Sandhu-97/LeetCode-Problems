c4b4a6140b8fabe1fa9d78dcd84dda844acab692
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-02-25 14:37:43 +0530

    Time: 4 ms (97.24%) | Memory: 11.9 MB (22.15%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -15,7 +15,6 @@ public:
         while (fast && fast->next){
             fast=fast->next->next;
             slow=slow->next;
-
             if (fast==slow) return true;
         }
         return false;
