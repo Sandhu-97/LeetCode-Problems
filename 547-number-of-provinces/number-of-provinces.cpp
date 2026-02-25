f2041d6124602ba9be5a916769285bcecc8e112e
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-02-25 13:56:12 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (63.88%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,32 +1,61 @@
-class Solution {
-public:
-    queue<int> q;
-    void bfs(int node, vector<vector<int>>& mat, vector<bool>& visited){
-        q.push(node);
-        visited[node] = true;
+class UF{
+    public:
+    vector<int> parent, rank;
 
-        while (!q.empty()){
-            int front = q.front();
-            q.pop();
-
-            for (int v=0;v<mat.size();v++){
-                if(mat[front][v]==1 && !visited[v]){
-                    visited[v] = true;
-                    q.push(v);
-                }
-            }
-        } 
+    UF(int n){
+        parent.resize(n);
+        rank.assign(n ,0);
+        for (int i=0;i<n;i++){
+            parent[i] = i;
+        }
     }
-    int findCircleNum(vector<vector<int>>& mat) {
-        int v = mat.size();
-        vector<bool> vis(v, false);
+
+    int find (int n){
+        if (parent[n]!=n) {
+            return parent[n]=find(parent[n]);
+        }
+        return parent[n];
+    }
+
+    void unite(int a, int b){
+        int pa = find(a);
+        int pb = find(b);
+
+        if (pa==pb) return;
+
+        if (rank[pa]>rank[pb]){
+            parent[pb] = pa;
+        }
+        else if (rank[pa]<rank[pb]){
+            parent[pa] = pb;
+        }
+        else{
+            parent[pb] = pa;
+             rank[pa]++;
+        }
+    }
+    int ans(int n){
         int count=0;
-        for(int i=0;i<mat.size();i++){
-            if (!vis[i]){
-                bfs(i, mat, vis);
-                count++;
-            }
+        for (int i=0;i<n;i++){
+            if (parent[i]==i) count++;
         }
         return count;
+    }
+};
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& conn) {
+        int n = conn.size();
+        UF uf(n);
+
+        for (int i=0;i<n;i++){
+            for (int j=i+1;j<n;j++){
+                if (conn[i][j]==1) uf.unite(i, j);
+            }
+        }
+
+        return uf.ans(n);
+
     }
 };
