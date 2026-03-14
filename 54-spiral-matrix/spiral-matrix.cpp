6795420b09ccbdccd27224dc9c7fd28a1cf09631
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-03-14 22:45:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (80.43%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int top=0, bottom=m-1;
-        int left=0, right=n-1;  
-        int dir=1;
-
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
         vector<int> ans;
-        while (top<=bottom && left<=right){
-            if(dir==1){
-                for(int i=left; i<=right;i++) ans.push_back(matrix[top][i]);
-                top++;
-                dir=2;
+        int m = mat.size();
+        int n = mat[0].size();
+
+        int top = 0;
+        int left = 0;
+        int down = m - 1;
+        int right = n - 1;
+
+        int dir = 1;
+        while (top <= down && left <= right) {
+
+            for (int i = left; i <= right; i++)
+                ans.push_back(mat[top][i]);
+            top++;
+
+            for (int i = top; i <= down; i++)
+                ans.push_back(mat[i][right]);
+            right--;
+
+            if (top <= down) {
+                for (int i = right; i >= left; i--)
+                    ans.push_back(mat[down][i]);
+                down--;
             }
-            else if(dir==2){
-                for (int i=top;i<=bottom;i++) ans.push_back(matrix[i][right]);
-                right--;
-                dir=3;
-            }
-            else if(dir==3){
-                for (int i=right;i>=left;i--) ans.push_back(matrix[bottom][i]);
-                bottom--;
-                dir=4; 
-            }
-            else if (dir==4){
-                for (int i=bottom;i>=top;i--) ans.push_back(matrix[i][left]);
+
+            if (left <= right) {
+                for (int i = down; i >= top; i--)
+                    ans.push_back(mat[i][left]);
                 left++;
-                dir=1;
             }
         }
-
         return ans;
     }
 };
