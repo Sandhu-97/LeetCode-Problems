6e8f872a46dcb7e557fb9e00a8f250a3ca15f791
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-04-27 22:29:07 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (36.65%) - LeetSync

---
+++ 3396-valid-word/valid-word.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool is_vowel(char c){
+        c=tolower(c);
+        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+    }
+    bool isValid(string word) {
+        int csts=0;
+        int vows=0;
+        for (char c: word){
+            if (!isdigit(c) && !isalpha(c)) return false;
+            else if (isalpha(c)){
+                if (is_vowel(c)) vows++;
+                else csts++;
+            }
+        }
+        return word.size()>=3 && csts>0 && vows>0;
+    }
+};
