72c8dec0f9b8648219063899c83a1e7e1442e28a
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-04-30 14:50:42 +0530

    Time: 3 ms (12.01%) | Memory: 22.9 MB (36.27%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
@@ -12,21 +12,19 @@
 class Solution {
 public:
     vector<int> largestValues(TreeNode* root) {
-        vector<int> ans;
         queue<TreeNode*> q;
-        if (!root) return {};
-        q.push(root);
-
+        vector<int> ans;
+        if (root) q.push(root);
         while (!q.empty()){
-            int size = q.size();
-            int val = INT_MIN;
+            int size=q.size();
+            int curr=INT_MIN;
             while (size--){
-                auto node = q.front(); q.pop();
-                val = max(val, node->val);
+                auto node=  q.front(); q.pop();
                 if (node->left) q.push(node->left);
                 if (node->right) q.push(node->right);
+                curr = max(curr, node->val);
             }
-            ans.push_back(val);
+            ans.push_back(curr);
         }
         return ans;
     }
