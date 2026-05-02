fe28f9b27bc29188cf805d4617af000e1358bb5a
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-05-02 14:16:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (27.09%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
-    unordered_map<char, string> mp;
-    int n;
-    string temp;
+    vector<string> opt={
+        "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
     vector<string> ans;
-
-    void solve(int i, string digits){
-        if (i>=n){
-            ans.push_back(temp);
+    void solve(int i, string digits, string& curr){
+        if (i>=digits.size()){
+            ans.push_back(curr);
             return;
-        }
+        };
 
-        string curr = mp[digits[i]];
-        for (char c: curr){
-            temp.push_back(c);
-            solve(i+1, digits);
-            temp.pop_back();
+        int digit = digits[i]-'2';
+        for (char option: opt[digit]){
+            curr.push_back(option);
+            solve(i+1, digits, curr);
+            curr.pop_back();
         }
-
         
     }
     vector<string> letterCombinations(string digits) {
-        mp['2'] = "abc";
-        mp['3'] = "def";
-        mp['4'] = "ghi";
-        mp['5'] = "jkl";
-        mp['6'] = "mno";
-        mp['7'] = "pqrs";
-        mp['8'] = "tuv";
-        mp['9'] = "wxyz";
-
-        n=digits.size();
-        solve(0, digits);
+        string curr="";
+        solve(0, digits, curr);
         return ans;
     }
 };
