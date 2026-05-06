3d2a2ee977403e33d85ee81d2ef914c1578ac49e
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-05-06 14:20:29 +0530

    Time: 900 ms (9.45%) | Memory: 11.5 MB (45.89%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
 
-    vector<int> dx = {1,-1,0,0};
-    vector<int> dy = {0,0,1,-1};
-    bool solve(int i, int j, int idx, int&m ,int& n, vector<vector<char>>& board, string& word){
+    bool solve(int idx, int i, int j, vector<vector<char>>& board, string word){
         if (idx>=word.size()) return true;
-        if (i>=m || i<0 || j<0 || j>=n || word[idx]!=board[i][j]) return false;
+        if (i<0 || j<0 || i>=board.size() || j>=board[0].size()||board[i][j]=='.') return false;
 
-        bool ans = false;
-        char temp = board[i][j];
-        board[i][j] = '.';
-        for (int k=0;k<4;k++){
-            if (solve(i+dx[k], j+dy[k], idx+1, m, n, board, word)){
-                board[i][j] = temp;
-                return true;
-            }
-        }
-        board[i][j] = temp;
-        return false;
+        if (board[i][j]!=word[idx]) return false;
+        char temp=board[i][j];
+        board[i][j]='.';
+        bool left = solve(idx+1, i, j-1, board, word);
+        bool right = solve(idx+1, i, j+1, board, word);
+        bool up = solve(idx+1, i-1, j, board, word);
+        bool down = solve(idx+1, i+1, j, board, word);
+        board[i][j]=temp;
+        return left||right||up||down;
+
     }
     bool exist(vector<vector<char>>& board, string word) {
-        
-        int m = board.size();
-        int n = board[0].size();
-
-        for (int i=0;i<m;i++){
-            for (int j=0;j<n;j++){
-                if (board[i][j] == word[0] && solve(i,j,0,m,n,board,word)) return true;
+        for (int i=0;i<board.size();i++){
+            for (int j=0;j<board[0].size();j++){
+                if (solve(0, i,j,board,word)) return true;
             }
         }
         return false;
