19e2579725e1db62caef9d33bf42fb8c77faa4ce
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-05-11 12:24:50 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (50.81%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int left=1;
-        int n=nums.size();
+        int left=1,
+        right=1;
 
-        for (int right=left;right<n;right++){
-            if (nums[left-1]!=nums[right]) nums[left++] = nums[right];
+        for(;right<nums.size();right++){
+            if (nums[right]==nums[right-1]) continue;
+            nums[left++]=nums[right];
         }
         return left;
     }
