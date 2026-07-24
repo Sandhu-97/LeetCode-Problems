83d48326df6bb526bd87e7d9951e30899fc2a266
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-07-24 14:07:51 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (76.37%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int start;
-    int end;
-    int m,n;
-    void dfs(int i, int j, vector<vector<int>>& image){
-        if (i<0 || j<0 || i>=m || j>=n || image[i][j]!=start) return;
-        image[i][j] = end;
-
-        dfs(i+1, j, image);
-        dfs(i, j+1, image);
-        dfs(i-1, j, image);
-        dfs(i, j-1, image);
+    int old_color, new_color, m, n;
+    void solve(int i, int j, vector<vector<int>>& image){
+        if (i<0 || j<0 || i>=m || j>=n || image[i][j]!=old_color) return;
+        image[i][j]=new_color;
+        solve(i+1, j, image);
+        solve(i-1, j, image);
+        solve(i, j+1, image);
+        solve(i, j-1, image);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        start = image[sr][sc];
-        end = color;
-        m = image.size(), n=image[0].size();
-        if (start==end) return image;
-        dfs(sr,sc,image);
+        m=image.size(), n=image[0].size();
+        old_color = image[sr][sc];
+        new_color = color;
+        if (old_color==new_color) return image;
+        solve(sr,sc,image);
         return image;
     }
 };
