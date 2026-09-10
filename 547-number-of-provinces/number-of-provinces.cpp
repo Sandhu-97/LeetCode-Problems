447a7e67ec897fd5643eab1aac560ca853202aa7
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-09-10 21:39:41 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (59.74%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,38 +1,47 @@
 class Solution {
 public:
-    vector<int> parent;
     vector<int> rank;
-    int find(int x){
-        if (parent[x]==x) return x;
-        return parent[x]=find(parent[x]);
-    }
+    vector<int> parent;
 
     void unite(int a, int b){
-        int pa=find(a), pb = find(b);
+        if (a==b) return;
+        int pa = find(a);
+        int pb = find(b);
+
         if (pa==pb) return;
+
         if (rank[pa]>rank[pb]) parent[pb]=pa;
         else if (rank[pb]>rank[pa]) parent[pa]=pb;
         else{
-            parent[pb]=pa;
-            rank[pa]++;
+            parent[pa]=pb;
+            rank[pb]++;
         }
     }
+    int find(int a){
+        if (parent[a]==a) return a;
+        return parent[a]=find(parent[a]);
+    }
+
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
         rank.resize(n, 0);
-        parent.resize(n);
-        for (int i=0;i<n;i++) parent[i]=i;
+        parent.resize(n, 0);
+        for (int i=0;i<n;i++) parent[i]=i; 
 
         for (int i=0;i<n;i++){
             for (int j=0;j<n;j++){
-                if (isConnected[i][j]==1) unite(i, j);
+                if (isConnected[i][j]){
+                    unite(i, j);
+                }
             }
         }
 
-        int ans=0;
+        int ans = 0;
         for (int i=0;i<n;i++){
             if (parent[i]==i) ans++;
         }
+
         return ans;
     }
+
 };
