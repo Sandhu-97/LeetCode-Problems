37dd5c23515aeff6222af86b81b04045d80554ad
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-09-11 14:21:26 +0530

    Time: 3 ms (25.73%) | Memory: 16.8 MB (50.57%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,46 +1,42 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int, int>> rotten;
-        int fresh=0, time=0;
+        int fresh=0, rotten=0, empty=0;
         int m = grid.size();
         int n = grid[0].size();
-        vector<int> dx = {0,0,1,-1};
+
+        vector<int> dx = {0,0,-1,1};
         vector<int> dy = {1,-1,0,0};
+
+        queue<pair<int,int>> q;
         for (int i=0;i<m;i++){
             for (int j=0;j<n;j++){
-                switch (grid[i][j]) {
-                    case 1:
-                    fresh++;
-                    break;
-                    
-                    case 2:
-                    rotten.push({i,j});
-                    break;
-
-                    default:
-                    break;
+                int cur = grid[i][j];
+                if (cur==0) empty++;
+                else if (cur==1) fresh++;
+                else{
+                    rotten++;
+                    q.push({i,j});
                 }
             }
         }
-
-        while (!rotten.empty()){
-            int size = rotten.size();
+        int time = 0;
+        while (!q.empty()){
+            int size = q.size();
             while (size--){
-                auto [i,j] = rotten.front(); rotten.pop();
+                auto [i,j] = q.front(); q.pop();
                 for (int k=0;k<4;k++){
-                    int nr = i+dx[k];
-                    int nc = j+dy[k];
+                    int newi = i+dx[k];
+                    int newj = j+dy[k];
 
-                    if (nr>=0 && nc >= 0 && nr<m && nc< n && grid[nr][nc]==1){
-                        grid[nr][nc]=2;
-                        fresh--;
-                        rotten.push({nr, nc});
-                    }
+                    if (newi<0 || newj<0 || newi>=m || newj >= n || grid[newi][newj]!=1) continue;
+                    q.push({newi, newj});
+                    grid[newi][newj] = 2;
+                    fresh--;
                 }
             }
-            if (!rotten.empty()) time++;
+            if (!q.empty()) time++;
         }
-        return fresh==0?time:-1;
+        return fresh==0 ? time : -1;
     }
 };
