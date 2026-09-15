ed20fd61060ad08dcc8708fb2bcdb3659be0ff5c
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2026-09-15 20:35:14 +0530

    Time: 8 ms (35.28%) | Memory: 20 MB (41.03%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    vector<int> visited;
-
-    bool cycle(int i, int parent, vector<vector<int>>& adj){
-        visited[i]=1;
-        for (auto neigh: adj[i]){
-            if (visited[neigh]==1) return true;
-            else if (visited[neigh]==0){
-                if (cycle(neigh, i, adj)) return true;
-            }
-        }
-        visited[i]=2;
-        return false;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
+        vector<int> inorder(numCourses, 0);
         for (auto edge: prerequisites){
             adj[edge[0]].push_back(edge[1]);
+            inorder[edge[1]]++;
         }
-        visited.resize(numCourses,0);
-
+        queue<int> q;
         for (int i=0;i<numCourses;i++){
-            if (visited[i]==0){
-                if (cycle(i, -1, adj)) return false;
-            }
+            if (inorder[i]==0) q.push(i);
         }
-        return true;
+        int count=0;
+        while (!q.empty()){
+            int front = q.front(); q.pop();
+            count++;
+            for (int neigh: adj[front]){
+                inorder[neigh]--;
+                if (inorder[neigh]==0) q.push(neigh);
+            }
+
+        }
+        return count==numCourses;
 
 
     }
